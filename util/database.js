d217289dd6dbdab1ce573747b94f5a39c80fc570
Aditya <aditya18.mail@gmail.com>
2022-12-04 11:00:15 +0530

    deployment

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
+
 const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME ,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    dialect:'mysql' ,
+    host:process.env.HOST,
+})
 
-const sequelize = new Sequelize('node-complete', 'user', '1808', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+module.exports = sequelize ;
